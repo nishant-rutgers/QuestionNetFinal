33105577e092e3603c3852db694222eb00c8cdec
nishant-rutgers <napartha@gmail.com>
2016-08-02 11:52:36 -0400

    Authentication

---
+++ app/src/main/java/com/example/sripriyanarayanprasad/questionnet/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.sripriyanarayanprasad.questionnet;
+
+/**
+ * Created by sripriyanarayanprasad on 8/1/16.
+ */
+public class AppConfig {
+    public static String URL_LOGIN = "http://173.72.11.102/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://173.72.11.102/android_login_api/register.php";
+
+}
+
--- app/src/main/java/com/example/sripriyanarayanprasad/questionnet/MainActivity.java
+++ app/src/main/java/com/example/sripriyanarayanprasad/questionnet/MainActivity.java
@@ -23,60 +23,39 @@ import android.view.MenuItem;
 import android.view.View;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements OnCameraChangeListener {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    public void toRestaurant(View view) {
-        Intent intent = new Intent(MainActivity.this, Restaurant.class);
-        startActivity(intent);
-    }
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+    private SQLiteHandler db;
+    private SessionManager session;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+
+
+
     private GoogleMap mMap;
-
-    /**
-     * Geofence Data
-     */
-
-    /**
-     * Geofences Array
-     */
     ArrayList<Geofence> mGeofences;
-
-    /**
-     * Geofence Coordinates
-     */
     ArrayList<LatLng> mGeofenceCoordinates;
-
-    /**
-     * Geofence Radius'
-     */
     ArrayList<Integer> mGeofenceRadius;
-
-    /**
-     * Geofence Store
-     */
     private GeofenceStore mGeofenceStore;
 
     @Override
@@ -86,6 +65,57 @@ public class MainActivity extends FragmentActivity implements OnCameraChangeList
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+
+
 
         // Initializing variables
         mGeofences = new ArrayList<Geofence>();
